7311b05c48527db83dc5415b5e6abc336e20284f
Travis <Travis-Byrman@live.nmit.ac.nz>
2020-06-15 12:48:39 +1200

    added code

---
+++ index.js
@@ -0,0 +1,35 @@
+// function slowHash(key) {
+// 	for(var i = 0; i < 10000; i++) {
+// 	console.log("everyday im hashing");
+//     }
+//     return key[0].charCodeAt(0);
+// }
+
+// function slowHash(key){
+//     for(let i = 0; i < 1000; i++){
+//         console.log("Everyday im hashing")
+//     }
+//     return key[0].charCodeAt(0)
+// }
+
+// function randomHash(key){
+//     return Math.floor(Math.random()*1000)
+// }
+
+// function hashExample(key){
+//     return 0 
+// }
+
+// function hash(ley, arrayLen) {
+//     let total = 0;
+//     for (let char of key) {
+//         // map "a" to 1, "b" to 2, "c" to 3, etc.
+//         let value = char.charCodeAt(0) - 96
+//         total = (total + value) % arrayLen;
+//     }
+//     return total;
+// }
+// hash("pink", 10); //0
+// hash("orangered", 10); // 7
+// hash("cyan", 10); //3
+
