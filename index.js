3ba71c014ae78e7662e3b1f3c98f4dde0e9341d6
Travis <Travis-Byrman@live.nmit.ac.nz>
2020-06-16 10:59:02 +1200

    Added increment example

--- index.js
+++ index.js
@@ -127,3 +127,45 @@ function addTwo(x) {
   return ret
 }
 console.log(addTwo(a))
+
+//--------------------------------------------------------------------------------------------
+
+let val1 = 2
+function multiplyThis(n) { // creating a new local execution context (calling a function)
+  let ret = n * val1
+  return ret
+}
+let multiplied = multiplyThis(6)
+console.log('Example of scope:', multiplied)
+
+//--------------------------------------------------------------------------------------------
+
+let val = 7 // declearing value assign a value
+function createAdder() {
+  function addNumbers(a,b) { // creating a new local execution context
+    let ret = a + b
+    return ret
+  }
+  return addNumbers
+}
+let adder = createAdder()
+let sum = adder(val,8)
+console.log('Example of function returning a function: ', sum)
+
+//--------------------------------------------------------------------------------------------
+
+function createCounter() {
+  let counter = 0
+  const myFunction = function() {
+    counter = counter + 1
+    return counter
+  }
+  return myFunction
+}
+const increment = createCounter()
+const c1 = increment()
+const c2 = increment()
+const c3 = increment()
+console.log('example increment', c1, c2, c3)
+
+//--------------------------------------------------------------------------------------------
